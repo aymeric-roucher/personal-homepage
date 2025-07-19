42bfe2cd001415c5c68b6fb5d8fe698b390c11cd
Aymeric <aymeric.roucher@gmail.com>
2025-07-18 20:18:49 -0700

    Update vite target

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/personal-homepage/',
+  base: '/personal-homepage/',
   build: {
     outDir: 'dist'
   },
