fb1524d985a1ce8a8718d1f3b49af08ac8a822f0
Aymeric Roucher <69208727+aymeric-roucher@users.noreply.github.com>
2025-07-18 20:04:31 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/' : '/repo-name/'
+  build: {
+    outDir: 'dist'
+  },
   server: {
     host: "::",
     port: 8080,
