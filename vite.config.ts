57f0573018436e2bf9914bb823932e12e4980d3c
Aymeric <aymeric.roucher@gmail.com>
2025-07-18 20:09:39 -0700

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/personal-homepage/'
+  base: process.env.NODE_ENV === 'production' ? '/' : '/personal-homepage/',
   build: {
     outDir: 'dist'
   },
